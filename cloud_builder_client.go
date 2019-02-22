85992a73e8e6c492b5f6e21f66f25f1cf11448d3
Anthony Metzidis <anthony.metzidis@gmail.com>
2019-02-22 15:58:07 -0800

    cleanup and project id

--- cloud_builder_client.go
+++ cloud_builder_client.go
@@ -2,49 +2,29 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
+	"os"
 
 	"golang.org/x/oauth2/google"
 	cloudbuild "google.golang.org/api/cloudbuild/v1"
 )
 
-func latestStatusWithContext(ctx context.Context, client *http.Client) ([]byte, error) {
+func latestStatusWithContext(ctx context.Context, client *http.Client) (string, error) {
 	svc, err := cloudbuild.New(client)
-	res, err := svc.Projects.Builds.List("fifth-coral-473").Do()
+	res, err := svc.Projects.Builds.List(os.Getenv("GOOGLE_CLOUD_PROJECT")).Do()
 	if err != nil {
 		log.Println(err)
-		return []byte{}, err
+		return "", err
 	}
 	if len(res.Builds) < 1 {
-		return []byte{}, errors.New("No Builds")
+		return "", errors.New("No Builds")
 	}
-	return json.Marshal(StatusToShieldJson(res.Builds[0].Status))
+	return res.Builds[0].Status, nil
 
 }
 
 func defaultGoogleClient(ctx context.Context) (*http.Client, error) {
 	return google.DefaultClient(ctx, cloudbuild.CloudPlatformScope)
 }
-
-/*
-func main() {
-	// Use oauth2.NoContext if there isn't a good context to pass in.
-	ctx := context.Background()
-
-	client, err := google.DefaultClient(ctx, compute.ComputeScope)
-	if err != nil {
-		log.Panic(err)
-		return
-	}
-	var status string
-	if status, err = latestStatusWithContext(&ctx, client); err != nil {
-		log.Panic(err)
-		return
-	}
-	log.Printf("Status = %s", status)
-}
-
-*/
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"google.golang.org/appengine"
@@ -23,6 +24,11 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unable to get status", 404)
 		return
 	}
+	jsonResponse, err := json.Marshal(StatusToShieldJson(latestStatus))
+	if err != nil {
+		http.Error(w, "Unable to encode json", 404)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(latestStatus)
+	w.Write(jsonResponse)
 }
